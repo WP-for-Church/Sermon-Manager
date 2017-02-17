bbc27b5c968663a1867dd90a85ebee2184eb09db
James <jamzth@gmail.com>
2017-02-16 23:32:32 -0600

    Bug Fixes

--- sermons.php
+++ sermons.php
@@ -3,7 +3,7 @@
 Plugin Name: Sermon Manager for WordPress
 Plugin URI: http://www.wpforchurch.com/products/sermon-manager-for-wordpress/
 Description: Add audio and video sermons, manage speakers, series, and more. Visit <a href="http://wpforchurch.com" target="_blank">Wordpress for Church</a> for tutorials and support.
-Version: 2.0.2
+Version: 2.0.3
 Author: WP for Church
 Contributors: wpforchurch, jprummer, jamzth
 Author URI: http://www.wpforchurch.com/
@@ -224,7 +224,7 @@ class SermonManager{
 	/**
 	 * Creates or returns an instance of this class.
 	 *
-	 * @return  Foo A single instance of this class.
+	 * @return  SermonManager A single instance of this class.
 	 */
 	public static function get_instance() {
 
