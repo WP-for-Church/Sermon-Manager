ef46f74d119d87b1ae5f95a24dce5eeb9273391f
Nikola <nikolam3244@gmail.com>
2017-09-27 01:35:33 +0200

    Show placeholder date on new sermon creation

--- includes/CMB2/includes/types/CMB2_Type_Text_Date.php
+++ includes/CMB2/includes/types/CMB2_Type_Text_Date.php
@@ -20,7 +20,7 @@ class CMB2_Type_Text_Date extends CMB2_Type_Picker_Base {
 			'value'           => isset( $_GET['post'] ) ? ( get_post_meta( $_GET['post'], 'sermon_date_auto', true ) ? '' : $this->field->get_timestamp_format() ) : '',
 			'desc'            => $this->_desc(),
 			'js_dependencies' => array( 'jquery-ui-core', 'jquery-ui-datepicker' ),
-			'placeholder'     => isset( $_GET['post'] ) && get_post_meta( $_GET['post'], 'sermon_date_auto', true ) ? $this->field->get_timestamp_format() : '',
+			'placeholder'     => isset( $_GET['post'] ) ? ( get_post_meta( $_GET['post'], 'sermon_date_auto', true ) ? $this->field->get_timestamp_format() : '' ) : $this->field->get_timestamp_format('date_format', time() + DAY_IN_SECONDS),
 		) );
 
 		if ( false === strpos( $args['class'], 'timepicker' ) ) {
