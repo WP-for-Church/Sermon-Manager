eeebbcd75183ffc4e9824cea50a870d15fc00fc8
Nikola <nikolam3244@gmail.com>
2017-10-26 19:49:35 +0200

    Add content to the page

--- includes/admin/class-sm-admin-import-export.php
+++ includes/admin/class-sm-admin-import-export.php
@@ -1,7 +1,23 @@
 <?php
 
 class SM_Admin_Import_Export {
-	public static function output(){
-		echo 'TODO';
+	public static function output() {
+		?>
+        <div class="sm wrap">
+            <div class="intro">
+                <h2>Sermon Manager Import/Export</h2>
+                <p>Hello, you can here import sermons from Sermon Manager export file (or other sermon plugins) or you
+                    can export data from Sermon Manager.</p>
+            </div>
+            <div class="actions">
+                <input type="button" class="button button-primary" value="Import from file" disabled>
+                <input type="button" class="button button-primary" value="Import from URL" disabled>
+                <input type="button" class="button" value="Export" disabled>
+                <p class="description">
+                    Note: Please make a backup of your database before importing, in case that something goes wrong.
+                </p>
+            </div>
+        </div>
+		<?php
 	}
 }
