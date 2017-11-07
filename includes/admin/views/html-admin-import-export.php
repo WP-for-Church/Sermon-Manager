219b4c407d5a85b6d2c6b62ff2a16f395647dd7a
Nikola <nikolam3244@gmail.com>
2017-11-07 15:08:07 +0100

    Fix importing layout

--- includes/admin/views/html-admin-import-export.php
+++ includes/admin/views/html-admin-import-export.php
@@ -2,9 +2,9 @@
 <div class="sm wrap">
     <div class="intro">
         <h1 class="wp-heading-inline">Sermon Manager Import/Export</h1>
-        <p>You can import/export content from and to Sermon Manager.</p>
     </div>
     <div class="wp-list-table widefat">
+        <p>You can import/export content from and to Sermon Manager.</p>
         <div id="the-list">
             <div class="plugin-card">
                 <div class="plugin-card-top">
@@ -44,53 +44,53 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div class="wp-list-table widefat">
         <h2>Import From 3rd Party Plugins</h2>
         <p>You can import content from other plugins into Sermon Manager.</p>
-        <div class="wp-list-table widefat">
-            <div id="the-list">
-                <div class="plugin-card">
-                    <div class="plugin-card-top">
-                        <div class="name column-name">
-                            <h3><a href="#">Sermon Browser
-                                    <span class="dashicons dashicons-editor-aligncenter plugin-icon"></a></a></a>
-                            </h3></div>
-                        <div class="action-links">
-                            <ul class="plugin-action-buttons">
-                                <li><a href="" class="button activate-now"
-                                       aria-label="Import from file">Import</a>
-                                </li>
-                                <li><a href="" class="" aria-label="More Details">More Details</a></li>
-                            </ul>
-                        </div>
-                        <div class="desc column-description">
-                            <p>Import from current database.</p>
-                        </div>
+        <div id="the-list">
+            <div class="plugin-card">
+                <div class="plugin-card-top">
+                    <div class="name column-name">
+                        <h3><a href="#">Sermon Browser
+                                <span class="dashicons dashicons-editor-aligncenter plugin-icon"></a></a></a>
+                        </h3></div>
+                    <div class="action-links">
+                        <ul class="plugin-action-buttons">
+                            <li><a href="" class="button activate-now"
+                                   aria-label="Import from file">Import</a>
+                            </li>
+                            <li><a href="" class="" aria-label="More Details">More Details</a></li>
+                        </ul>
+                    </div>
+                    <div class="desc column-description">
+                        <p>Import from current database.</p>
                     </div>
                 </div>
-                <div class="plugin-card">
-                    <div class="plugin-card-top">
-                        <div class="name column-name">
-                            <h3><a href="#">Series Engine
-                                    <span class="dashicons dashicons-editor-aligncenter plugin-icon"></a></a></a>
-                            </h3></div>
-                        <div class="action-links">
-                            <ul class="plugin-action-buttons">
-                                <li><a href="" class="button activate-now"
-                                       aria-label="Import from file">Import</a>
-                                </li>
-                                <li><a href="" class="" aria-label="More Details">More Details</a></li>
-                            </ul>
-                        </div>
-                        <div class="desc column-description">
-                            <p>Import from current database. Note: Series Types will not be imported, since
-                                Sermon Manager does not have them.</p>
-                        </div>
+            </div>
+            <div class="plugin-card">
+                <div class="plugin-card-top">
+                    <div class="name column-name">
+                        <h3><a href="#">Series Engine
+                                <span class="dashicons dashicons-editor-aligncenter plugin-icon"></a></a></a>
+                        </h3></div>
+                    <div class="action-links">
+                        <ul class="plugin-action-buttons">
+                            <li><a href="" class="button activate-now"
+                                   aria-label="Import from file">Import</a>
+                            </li>
+                            <li><a href="" class="" aria-label="More Details">More Details</a></li>
+                        </ul>
+                    </div>
+                    <div class="desc column-description">
+                        <p>Import from current database. Note: Series Types will not be imported, since
+                            Sermon Manager does not have them.</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <p class="description">
-        Note: Please make a backup of your database before importing, in case that something goes wrong.
-    </p>
 </div>
+<p class="description">
+    Note: Please make a backup of your database before importing, in case that something goes wrong.
+</p>
