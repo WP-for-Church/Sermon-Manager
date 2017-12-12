ecf63dad99aaea6f80e57b1f5c77c30091f75d91
Nikola <nikolam3244@gmail.com>
2017-12-12 18:24:32 +0100

    Fix development version update functions not executing

--- sermons.php
+++ sermons.php
@@ -69,7 +69,6 @@ class SermonManager {
 		// Include required items
 		$this->_includes();
 
-		register_activation_hook( __FILE__, array( $this, 'check_for_update_functions' ) );
 		// load translations
 		add_action( 'after_setup_theme', array( $this, 'load_translations' ) );
 		// enqueue scripts & styles
@@ -419,19 +418,6 @@ class SermonManager {
 	 */
 	function php_notice_handler() {
 		update_option( 'dismissed-' . $_POST['type'], 1 );
-	}
-
-	/**
-	 * Executes non-executed update functions on plugin activation
-	 *
-	 * Useful for development versions
-	 *
-	 * @since 2.9.3
-	 */
-	function check_for_update_functions() {
-		$GLOBALS['sm_force_update'] = true;
-
-		include_once 'includes/class-sm-install.php';
 	}
 }
 
