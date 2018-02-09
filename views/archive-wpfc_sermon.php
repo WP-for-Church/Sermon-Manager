d27a0946397b1f3d7d7af2db4b77f143c9a787d5
Adrian Ionescu <9744973+adrianweb@users.noreply.github.com>
2018-02-09 14:13:57 +0000

    initial v2 views draft

--- views/archive-wpfc_sermon.php
+++ views/archive-wpfc_sermon.php
@@ -1,57 +1,32 @@
 <?php
-/**
- * The template for displaying archive pages
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
- */
-
 get_header(); ?>
 
-<div class="wrap">
-
-	<?php if ( have_posts() ) : ?>
-		<header class="page-header">
+<div id="" class="wrap">
+    <div id="primary" class="">
+        <main id="" class="wpfc-sermon-archive">
+            
 			<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="taxonomy-description">', '</div>' );
+			if ( have_posts() ) :
+
+				while ( have_posts() ) : the_post();
+
+					wpfc_sermon_excerpt_v2();
+
+				endwhile;
+
+				the_posts_pagination();
+
+			else :
+
+				__('Sorry, but there aren\'t any posts matching your query.', 'placeholder');
+
+			endif;
 			?>
-		</header><!-- .page-header -->
-	<?php endif; ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php
-		if ( have_posts() ) : ?>
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				wpfc_sermon_excerpt();
-
-			endwhile;
-
-			the_posts_pagination();
-		else :
-
-			get_template_part( 'template-parts/post/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-	<?php get_sidebar(); ?>
-</div><!-- .wrap -->
+        </main>
+    </div>
+    
+    <?php get_sidebar(); ?>
+</div>
 
 <?php get_footer();
-
