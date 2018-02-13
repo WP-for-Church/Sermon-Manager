aba8ac2b221d8561ce448bfc4f1e77f7c78741e1
Nikola <nikolam3244@gmail.com>
2018-02-13 10:11:46 +0100

    Add wrapper partials

--- views/archive-wpfc_sermon.php
+++ views/archive-wpfc_sermon.php
@@ -1,56 +1,17 @@
+<?php get_header(); ?>
+
+<?php include 'partials/wrapper-start.php'; ?>
+
+<?php echo render_wpfc_sorting(); ?>
 <?php
-get_header(); ?>
+if ( have_posts() ) :
+	while ( have_posts() ) : the_post();
+		wpfc_sermon_excerpt_v2();
+	endwhile;
+	the_posts_pagination();
+else :
+	__( 'Sorry, but there aren\'t any posts matching your query.', 'placeholder' );
+endif;
+?>
 
-<?php if ( wp_get_theme() == 'Divi' ) : ?>
-
-    <div id="main-content">
-        <div class="container">
-        	<div id="content-area" class="clearfix">
-                <main id="left-area" class="wpfc-sermon-archive">
-
-        	        <?php echo render_wpfc_sorting(); ?>
-            
-			        <?php
-			        if ( have_posts() ) :
-				        while ( have_posts() ) : the_post();
-					        wpfc_sermon_excerpt_v2();
-				        endwhile;
-				        the_posts_pagination();
-		             else :
-				        __('Sorry, but there aren\'t any posts matching your query.', 'placeholder');
-			        endif;
-			        ?>
-                </main>
-                <?php get_sidebar(); ?>
-            </div>
-        </div>
-    </div>
-   
-<?php else: ?>
-
-    <div class="wrap">
-        <div id="primary">
-            <main class="wpfc-sermon-archive">
-
-        	    <?php echo render_wpfc_sorting(); ?>
-            
-			    <?php
-			    if ( have_posts() ) :
-				    while ( have_posts() ) : the_post();
-					    wpfc_sermon_excerpt_v2();
-				    endwhile;
-				    the_posts_pagination();
-		         else :
-				    __('Sorry, but there aren\'t any posts matching your query.', 'placeholder');
-			    endif;
-			    ?>
-
-            </main>
-        </div>
-    
-        <?php get_sidebar(); ?>
-    </div>
-
-<?php endif; ?>
-
-<?php get_footer();
+<?php include 'partials/wrapper-end.php'; ?>
---
+++ views/partials/wrapper-end.php
@@ -0,0 +1,35 @@
+<?php defined( 'ABSPATH' ) or exit;
+
+$template = get_option( 'template' );
+
+switch ( $template ) {
+	case 'twentyeleven' :
+		echo '</div>';
+		get_sidebar();
+		echo '</div>';
+		break;
+	case 'twentytwelve' :
+		echo '</div></div>';
+		break;
+	case 'twentythirteen' :
+		echo '</div></div>';
+		break;
+	case 'twentyfourteen' :
+		echo '</div></div></div>';
+		get_sidebar( 'content' );
+		break;
+	case 'twentyfifteen' :
+		echo '</div></div>';
+		break;
+	case 'twentysixteen' :
+		echo '</main></div>';
+		break;
+	case 'Divi':
+		echo '</main>';
+		get_sidebar();
+		echo '</div></div></div>';
+		break;
+	default :
+		echo '</div></div>';
+		break;
+}
