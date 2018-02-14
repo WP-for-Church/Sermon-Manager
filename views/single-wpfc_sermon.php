043b5c9cc5f4b9d3b16189667bf701ae3b8d574b
Adrian Ionescu <9744973+adrianweb@users.noreply.github.com>
2018-02-14 07:09:44 +0000

    improve theme compatibiliy

--- views/single-wpfc_sermon.php
+++ views/single-wpfc_sermon.php
@@ -1,46 +1,17 @@
 <?php
 get_header(); ?>
 
-<?php if ( wp_get_theme() == 'Divi' ) : ?>
+<?php include 'partials/wrapper-start.php'; ?>
 
-    <div id="main-content">
-    	<div class="container">
-    		<div id="content-area" class="clearfix">
-    			<main id="left-area" class="wpfc-sermon-singular">
-			        <?php
-			        while ( have_posts() ) : the_post();
-				        wpfc_sermon_single_v2();
-				        if ( comments_open() || get_comments_number() ) :
-					        comments_template();
-				        endif;
-			        endwhile;
-			        ?>
-    			</main>
-    		</div>
-    	</div>
-    </div>
+<?php
+	while ( have_posts() ) : the_post();
+		wpfc_sermon_single_v2();
+		if ( comments_open() || get_comments_number() ) :
+			comments_template();
+		endif;
+	endwhile;
+?>
 
-<?php else: ?>
-
-    <div class="wrap">
-	    <div id="primary">
-		    <main class="wpfc-sermon-singular">
-
-			    <?php
-			    while ( have_posts() ) : the_post();
-				    wpfc_sermon_single_v2();
-				    if ( comments_open() || get_comments_number() ) :
-					    comments_template();
-				    endif;
-			    endwhile;
-			    ?>
-
-		    </main>
-	    </div>
-
-	    <?php get_sidebar(); ?>
-    </div>
-
-<?php endif; ?>
+<?php include 'partials/wrapper-end.php'; ?>
 
 <?php get_footer();
