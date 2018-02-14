d0ec4b7fb7a64694a40e4d7b2cef8160b1027f58
Adrian Ionescu <9744973+adrianweb@users.noreply.github.com>
2018-02-14 07:13:19 +0000

    more generic wrapper class, so it works for both archive and single view

--- views/partials/wrapper-start.php
+++ views/partials/wrapper-start.php
@@ -4,30 +4,30 @@ $template = get_option( 'template' );
 
 switch ( $template ) {
 	case 'twentyeleven' :
-		echo '<div id="primary"><div id="content" role="main" class="wpfc-sermon-archive">';
+		echo '<div id="primary"><div id="content" role="main" class="wpfc-sermon-container">';
 		break;
 	case 'twentytwelve' :
-		echo '<div id="primary" class="site-content"><div id="content" role="main" class="wpfc-sermon-archive">';
+		echo '<div id="primary" class="site-content"><div id="content" role="main" class="wpfc-sermon-container">';
 		break;
 	case 'twentythirteen' :
-		echo '<div id="primary" class="content-area"><div id="content" role="main" class="site-content wpfc-sermon-archive">';
+		echo '<div id="primary" class="content-area"><div id="content" role="main" class="site-content wpfc-sermon-container">';
 		break;
 	case 'twentyfourteen' :
 		echo '<div id="main-content" class="main-content"><div id="primary" class="content-area"><div id="content" class="site-content" role="main">';
 		break;
 	case 'twentyfifteen' :
-		echo '<div id="primary" class="content-area"><div id="main" role="main" class="site-main wpfc-sermon-archive">';
+		echo '<div id="primary" class="content-area"><div id="main" role="main" class="site-main wpfc-sermon-container">';
 		break;
 	case 'twentysixteen' :
-		echo '<div id="primary" class="content-area"><main id="main" class="site-main wpfc-sermon-archive" role="main">';
+		echo '<div id="primary" class="content-area"><main id="main" class="site-main wpfc-sermon-container" role="main">';
 		break;
 	case 'Divi':
-		echo '<div id="main-content"><div class="container"><div id="content-area" class="clearfix"><main id="left-area" class="wpfc-sermon-archive">';
+		echo '<div id="main-content"><div class="container"><div id="content-area" class="clearfix"><main id="left-area" class="wpfc-sermon-container">';
 		break;
 	case 'salient' :
 		echo '<div class="container-wrap"><div class="container main-container"><div class="row"><div class="post-area col span_9"><div class="post-container">';
 		break;
 	default :
-		echo '<div class="wrap"><div id="primary" class="content-area"><main id="main" class="site-main wpfc-sermon-archive">';
+		echo '<div class="wrap"><div id="primary" class="content-area"><main id="main" class="site-main wpfc-sermon-container">';
 		break;
 }
