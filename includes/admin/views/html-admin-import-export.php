66a9071861c1d432859f0fd6d8843d61a125efe9
Uroš Milanović <urosmil@hotmail.com>
2018-03-01 21:31:33 +0000

    Adjust importing based on feedback

--- includes/admin/views/html-admin-import-export.php
+++ includes/admin/views/html-admin-import-export.php
@@ -17,6 +17,41 @@
                     </div>
                     <div class="action-links">
                         <ul class="plugin-action-buttons">
+                            <li>
+                            <?php 
+                                $bytes = apply_filters( 'import_upload_size_limit', wp_max_upload_size() );
+                            	$size = size_format( $bytes );
+                            	$upload_dir = wp_upload_dir();
+                            	if ( ! empty( $upload_dir['error'] ) ) : ?>
+                            		<div class="error">
+                            		    <p>
+                            		        <?php esc_html_e('Before you can upload your import file, you will need to fix the following error:', 'sermon-manager-for-wordpress'); ?>
+                        		        </p>
+                            		    <p>
+                            		        <strong>
+                            		            <?php echo $upload_dir['error']; ?>
+                        		            </strong>
+                    		            </p>
+                		            </div>
+            		            <?php else : ?>
+                                    <form enctype="multipart/form-data" id="sm-import-upload-form" method="post" class="wp-upload-form" action="<?php echo esc_url( wp_nonce_url( $_SERVER['REQUEST_URI'] . '&doimport=sm', 'sm' ) ); ?>">
+                                        <p>
+                                            <input type="file" id="upload" name="import" size="25" />
+                                            <input type="hidden" name="action" value="save" />
+                                            <input type="hidden" name="max_file_size" value="<?php echo $bytes; ?>" />
+                                        </p>
+                                        <input class="button" id="submit" type="submit" name="submit" value="<?php esc_attr_e( 'Import from file', 'sermon-manager-for-wordpress' ) ?>" />
+                                    </form>
+                                    <span class="button activate-now" id="sm-import-trigger">
+                                        <span>
+                                            <?php _e( 'Import', 'sermon-manager-for-wordpress' ) ?>
+                                        </span>
+                                        <span class="import-sniper">
+                                            <img src="<?php echo admin_url( 'images/wpspin_light.gif' ); ?>">
+                                          </span>
+                                    </span>
+                                <?php endif; ?>
+                            </li>
                             <li><a href="" class=""
                                    aria-label="<?php esc_attr_e( 'More Details', 'sermon-manager-for-wordpress' ) ?>">
 									<?php _e( 'More Details', 'sermon-manager-for-wordpress' ) ?>
@@ -25,33 +60,6 @@
                     </div>
                     <div class="desc column-description">
                         <p><?php _e( 'Import sermons from another Sermon Manager installation.', 'sermon-manager-for-wordpress' ) ?></p>
-                        <div>
-                        <?php 
-                            $bytes = apply_filters( 'import_upload_size_limit', wp_max_upload_size() );
-                        	$size = size_format( $bytes );
-                        	$upload_dir = wp_upload_dir();
-                        	if ( ! empty( $upload_dir['error'] ) ) : ?>
-                        		<div class="error">
-                        		    <p>
-                        		        <?php esc_html_e('Before you can upload your import file, you will need to fix the following error:', 'sermon-manager-for-wordpress'); ?>
-                    		        </p>
-                        		    <p>
-                        		        <strong>
-                        		            <?php echo $upload_dir['error']; ?>
-                    		            </strong>
-                		            </p>
-            		            </div>
-        		            <?php else : ?>
-                                <form enctype="multipart/form-data" id="sm-import-upload-form" method="post" class="wp-upload-form" action="<?php echo esc_url( wp_nonce_url( $_SERVER['REQUEST_URI'] . '&doimport=sm', 'sm' ) ); ?>">
-                                    <p>
-                                        <input type="file" id="upload" name="import" size="25" />
-                                        <input type="hidden" name="action" value="save" />
-                                        <input type="hidden" name="max_file_size" value="<?php echo $bytes; ?>" />
-                                    </p>
-                                    <input class="button" type="submit" name="submit" value="<?php esc_attr_e( 'Import from file', 'sermon-manager-for-wordpress' ) ?>" />
-                                </form>
-                            <?php endif; ?>
-                        </div>
                     </div>
                 </div>
             </div>
@@ -66,6 +74,12 @@
                     </div>
                     <div class="action-links">
                         <ul class="plugin-action-buttons">
+                            <li>
+							    <a href="<?php echo $_SERVER['REQUEST_URI'] ?>&doimport=exsm" class="button activate-now" id="sm-export-content"
+                               aria-label="<?php esc_attr_e( 'Export to file', 'sermon-manager-for-wordpress' ) ?>">
+								<?php _e( 'Export', 'sermon-manager-for-wordpress' ) ?>
+                            </a>
+							</li>
                             <li><a href="" class=""
                                    aria-label="<?php esc_attr_e( 'More Details', 'sermon-manager-for-wordpress' ) ?>">
 									<?php _e( 'More Details', 'sermon-manager-for-wordpress' ) ?></a></li>
@@ -73,12 +87,6 @@
                     </div>
                     <div class="desc column-description">
                         <p><?php _e( 'Create an export for the purpose of backup or migration to another website.', 'sermon-manager-for-wordpress' ) ?></p>
-                        <div class="plugin-card-export-btn-holder">
-                            <a href="<?php echo $_SERVER['REQUEST_URI'] ?>&doimport=exsm" class="button activate-now" id="sm-export-content"
-                               aria-label="<?php esc_attr_e( 'Export to file', 'sermon-manager-for-wordpress' ) ?>">
-								<?php _e( 'Export to file', 'sermon-manager-for-wordpress' ) ?>
-                            </a>
-                        </div>
                     </div>
                 </div>
             </div>
