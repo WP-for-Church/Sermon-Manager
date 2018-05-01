221d5645fa26bcbeb9c9bc8adc7ef380f5cfeb2e
Robert Main <lenswipe@gmail.com>
2018-04-30 20:16:02 -0400

    Convert intentation to tabs

--- includes/class-sm-post-types.php
+++ includes/class-sm-post-types.php
@@ -262,7 +262,7 @@ class SM_Post_Types {
 				'entry-views',
 				'elementor',
 				( ! \SermonManager::getOption( 'post_excerpt_enabled', 1 ) ? 'excerpt' : null ),
-				'revisions'
+				'revisions',
 			),
 		) ) );
 
