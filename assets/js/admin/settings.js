ce3ccfe98358a0430cb548478e564d66ad207214
zSeriesGuy <jerry@nance.us>
2018-06-14 09:32:45 -0400

    Add js to select default image
    Add js to select default image and to refresh to thumbnail view.

--- assets/js/admin/settings.js
+++ assets/js/admin/settings.js
@@ -1,4 +1,35 @@
 /**
+ * Media Chooser
+ */
+jQuery(document).ready(function($){
+    var custom_uploader;
+    $('#upload_default_image').click(function(e) {
+        e.preventDefault();
+        //If the uploader object has already been created, reopen the dialog
+        if (custom_uploader) {
+            custom_uploader.open();
+            return;
+        }
+        //Extend the wp.media object
+        custom_uploader = wp.media.frames.file_frame = wp.media({
+            title: 'Choose Image',
+            button: {
+                text: 'Choose Image'
+            },
+            multiple: false
+        });
+        //When a file is selected, grab the URL and set it as the text field's value
+        custom_uploader.on('select', function() {
+            attachment = custom_uploader.state().get('selection').first().toJSON();
+            $('#default_image').val(attachment.url);
+			$("#default-image-thumb-load").html('<br /><img style="width: inherit;" src="' + attachment.url + '" />');
+        });
+        //Open the uploader dialog
+        custom_uploader.open();
+    });
+});
+
+/**
  * Hide or show fields on checkbox change
  */
 jQuery('#enable_podcast_redirection').change(function () {
