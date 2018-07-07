012fc50595656c045905c59f19e4365c86f1bd8d
Nikola Miljković <nikolam3244@gmail.com>
2018-07-07 23:25:44 +0200

    Fix CS and logic in archive view

--- views/archive-wpfc_sermon.php
+++ views/archive-wpfc_sermon.php
@@ -7,19 +7,24 @@
 
 get_header(); ?>
 
-<?php echo wpfc_get_partial('content-sermon-wrapper-start'); ?>
+<?php echo wpfc_get_partial( 'content-sermon-wrapper-start' ); ?>
 
 <?php echo render_wpfc_sorting(); ?>
 <?php
 if ( have_posts() ) :
-	if ( wpfc_sm_pro_is_templating_being_used() ) :
-	echo '<div class="smpro-items">';
-	    while ( have_posts() ) :
-		    the_post();
-		    wpfc_sermon_excerpt_v2(); // You can edit the content of this function in `partials/content-sermon-archive.php`.
-	    endwhile;
-	echo '</div>';
+	if ( function_exists( 'wpfc_sm_pro_is_templating_being_used' ) && wpfc_sm_pro_is_templating_being_used() ) :
+		echo '<div class="smpro-items">';
 	endif;
+
+	while ( have_posts() ) :
+		the_post();
+		wpfc_sermon_excerpt_v2(); // You can edit the content of this function in `partials/content-sermon-archive.php`.
+	endwhile;
+
+	if ( function_exists( 'wpfc_sm_pro_is_templating_being_used' ) && wpfc_sm_pro_is_templating_being_used() ) :
+		echo '</div>';
+	endif;
+
 	if ( function_exists( 'wp_pagenavi' ) ) :
 		wp_pagenavi();
 	else :
@@ -30,7 +35,7 @@ else :
 endif;
 ?>
 
-<?php echo wpfc_get_partial('content-sermon-wrapper-end'); ?>
+<?php echo wpfc_get_partial( 'content-sermon-wrapper-end' ); ?>
 
 <?php
 get_footer();
