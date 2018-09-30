51a45b943fbd6e15898632b10c287157bc8fa33e
Nikola Miljković <nikolam3244@gmail.com>
2018-09-30 03:30:59 +0200

    Remove unneeded code

--- includes/admin/class-sm-admin.php
+++ includes/admin/class-sm-admin.php
@@ -18,7 +18,6 @@ class SM_Admin {
 	 */
 	public function __construct() {
 		add_action( 'init', array( $this, 'includes' ) );
-		add_action( 'current_screen', array( $this, 'conditional_includes' ) );
 		add_action( 'admin_init', array( $this, 'buffer' ), 1 );
 	}
 
@@ -36,14 +35,6 @@ class SM_Admin {
 		include_once 'sm-admin-functions.php';
 		include_once 'class-sm-admin-post-types.php';
 		include_once 'class-sm-admin-menus.php';
-		include_once 'class-sm-admin-assets.php';
-	}
-
-	/**
-	 * Include admin files conditionally.
-	 */
-	public function conditional_includes() {
-
 	}
 }
 
