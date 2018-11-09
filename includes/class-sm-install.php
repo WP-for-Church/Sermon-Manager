378e26b21980f32cb057414c2e4836e25f7afffe
Nikola Miljković <nikolam3244@gmail.com>
2018-11-09 06:19:24 +0100

    Add SMP mentions

--- includes/class-sm-install.php
+++ includes/class-sm-install.php
@@ -267,6 +267,7 @@ class SM_Install {
 		if ( SM_BASENAME == $file ) {
 			$row_meta = array(
 				'support' => '<a href="' . esc_url( 'https://wpforchurch.com/my/submitticket.php?utm_source=sermon-manager&utm_medium=wordpress' ) . '" aria-label="' . esc_attr__( 'Visit premium customer support', 'sermon-manager-for-wordpress' ) . '">' . esc_html__( 'Premium support', 'sermon-manager-for-wordpress' ) . '</a>',
+				'smp'     => '<a href="https://sermonmanager.pro/?utm_source=sermon-manager&amp;utm_medium=wordpress" aria-label="' . esc_attr( __( 'Get Sermon Manager Pro', 'sermon-manager-pro' ) ) . '" target="_blank" style="color:#ff0000;">' . __( 'Get Sermon Manager Pro', 'sermon-manager-pro' ) . '</a>',
 			);
 
 			return array_merge( $links, $row_meta );
